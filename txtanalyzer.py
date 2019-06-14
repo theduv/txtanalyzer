c1c3d71cea7c398a7bbf00b47414dcdc25a37c0c
theduv <tduval@student.42.fr>
2019-06-14 03:47:51 +0200

    to do: split on all whitespaces

--- txtanalyzer.py
+++ txtanalyzer.py
@@ -3,21 +3,24 @@ import os
 
 def print_usage() :
     print("txtanalyzer: ./txtanalyzer [file]")
-if len(sys.argv) != 2 :
+if len(sys.argv) < 2 :
     print_usage()
     sys.exit(1)
-if os.path.exists(sys.argv[1]) :
-    fd1 = open(sys.argv[1], "r")
+content = []
+for arg in sys.argv[1:] :
+    if os.path.exists(arg) and os.path.isfile(arg):
+        fd = open(arg, "r")
+        if fd == -1 :
+            sys.stderr.write("Error: could not open " + arg)
+        else :
+            content += list(filter(bool, fd.read().split('\n')))
+            str = ' '.join(content)
+            content += list(filter(bool, str.split(' ')))
 if os.path.exists("./skiplist.txt") :
     fd2 = open("skiplist.txt", "r")
-if fd1 == -1 :
-    sys.stderr.write("Error: could not open " + sys.argv[1])
 if 'fd2' in locals() and fd2 != -1 :
     skiplist = list(filter(bool, fd2.read().split('\n')))
 terms = {}
-content = list(filter(bool, fd1.read().split('\n')))
-str = ' '.join(content)
-content = list(filter(bool, str.split(' ')))
 for word in content :
     if word in terms.keys() :
         terms[word] += 1
@@ -25,4 +28,4 @@ for word in content :
         terms[word] = 1
 for key, value in sorted(terms.items(), reverse=True, key=lambda item: item[1]) :
     if 'skiplist' not in locals() or key not in skiplist :
-        print("%s : %s" % (key, value))
+        print("%s : %s" % (key, terms[key]))
